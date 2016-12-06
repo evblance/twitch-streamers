eee378250b59e683649f1ea956322ff4fad949d6
evblance <evblance@gmail.com>
2016-12-06 16:27:30 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas","comster404"];
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas","comster404"];
 
 function getStatus(user) {
-  channelUrl = 'https://api.twitch.tv/kraken/channels/' + user;
+  //channelUrl = 'https://api.twitch.tv/kraken/channels/' + user;
+  channelUrl = 'https://wind-bow.gomix.me/twitch-api/channels/' + user;
 
+  
   $.ajax({
     type: 'GET',
-    dataType: 'json',
+    //dataType: 'json',
+    dataType: 'jsonp',
     contentType: 'application/json; charset=utf-8',
     url: channelUrl,
     success: function(data) {
@@ -21,7 +24,9 @@ function getStatus(user) {
         userDataHtml += "<div class='offline-user user'>";
       }
       if (data.logo !== null) {
-        userDataHtml += "<img src='"+data.logo+"' class='user-img'/>";  
+        var userImage = 'http' + data.logo.slice(5,data.logo.length);
+        console.log(userImage);
+        userDataHtml += "<img src='"+userImage+"' class='user-img'/>";  
       }
       userDataHtml += "<a class='user-link' href='"+data.url+"' target='_blank'>";
       userDataHtml += ""+data.display_name+"</a>";
@@ -46,7 +51,10 @@ function getStatus(user) {
         userDataHtml += "</div>";
         $(userDataHtml).appendTo($('#users-panel'));
       } else {
-        alert('An unknown error has occurred.');
+        console.log(e.message);
+        console.log(e.status);
+        console.log(e.error);
+        alert('An API error has occurred.');
       }
     }
   });

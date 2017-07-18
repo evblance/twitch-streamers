7193984113c87e3cd357aad2c5b2497926a73f79
evblance <evblance@users.noreply.github.com>
2017-07-18 23:19:07 +1000

    Refactors
    - Overhauls code, turning into a single-page application using Angular
    - Changes some styles and improves scalability and maintainability
    - General improvements to code quality and style
    - Updates README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# TwitchTV Streamers
+# Twitch.tv Streamers
+A project using the Twitch.tv JSON API, built for FreeCodeCamp Front-End Curriculum.
+
+## How to Run
+### Online
+Simply navigate [here](https://evblance.github.io/twitch-streamers/).
+
+### Offline
+1. Download or clone this repository to your computer
+2. Run `index.html` in your favourite browser.
--- main.js
+++ main.js
@@ -1,95 +1,116 @@
-var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas","comster404"];
+var app = angular.module('twitchApp', []);
 
-function getStatus(user) {
-  //channelUrl = 'https://api.twitch.tv/kraken/channels/' + user;
-  channelUrl = 'https://wind-bow.gomix.me/twitch-api/channels/' + user;
+app.controller('twitchCtrl', function($scope, $http) {
 
-  
-  $.ajax({
-    type: 'GET',
-    //dataType: 'json',
-    dataType: 'jsonp',
-    contentType: 'application/json; charset=utf-8',
-    url: channelUrl,
-    success: function(data) {
-      var accountClosed = false;
-      var streamAvailable = false;
-      var userDataHtml = "";
+  $scope.baseUrl = 'https://wind-bow.gomix.me/twitch-api/channels/';
 
-      // check if user is online
-      if (data.partner === true) {
-        userDataHtml += "<div class='online-user user'>";
-        streamAvailable = true;
-      } else {
-        userDataHtml += "<div class='offline-user user'>";
-      }
-      if (data.logo !== null) {
-        var userImage = 'http' + data.logo.slice(5,data.logo.length);
-        console.log(userImage);
-        userDataHtml += "<img src='"+userImage+"' class='user-img'/>";  
-      }
-      userDataHtml += "<a class='user-link' href='"+data.url+"' target='_blank'>";
-      userDataHtml += ""+data.display_name+"</a>";
-      if (!streamAvailable) {
-        userDataHtml += "<br><p class='offline-user-description'>User Offline</p>";
-      } else {
-        userDataHtml += "<br><p class='online-user-description'>"+data.status+"</p>";
-      }
-      userDataHtml += "</div>";
-      $(userDataHtml).appendTo($('#users-panel'));
-    },
-    error: function(e) {
-      // check if user has closed their account
-      if (e.statusText == "Unprocessable Entity") {
-        var userDataHtml = "";
-        
-        userDataHtml += "<div class='offline-user user'>";
-        userDataHtml += "<img src='http://vignette3.wikia.nocookie.net/spore/images/6/6c/Question-mark.png/revision/latest?cb=20110427230528' class='user-img'/>"; 
-        userDataHtml += "<p style='display:inline;padding-left:10px' class='user-link'>";
-        userDataHtml += ""+user+"</p>";
-        userDataHtml += "<br><p class='offline-user-description'>Account Closed</p>";
-        userDataHtml += "</div>";
-        $(userDataHtml).appendTo($('#users-panel'));
-      } else {
-        console.log(e.message);
-        console.log(e.status);
-        console.log(e.error);
-        alert('An API error has occurred.');
+  $scope.streamerArr = [
+    "ESL_SC2",
+    "OgamingSC2",
+    "cretetion",
+    "storbeck",
+    "freecodecamp",
+    "habathcx",
+    "RobotCaleb",
+    "noobs2ninjas"
+  ];
+
+  $scope.loadingStatus = true;
+
+  $scope.allStreamers = [];
+
+  $scope.onlineStreamers = [];
+
+  $scope.offlineStreamers = [];
+
+  $scope.activeTab = 1;
+
+  // function that checks whether the refresh button should be spinning
+  $scope.adjustLoadingStatus = function() {
+    if ($scope.streamerArr.length === $scope.allStreamers.length) {
+      $scope.loadingStatus = false;
+      return;
+    }
+    $scope.loadingStatus = true;
+  };
+
+  $scope.streamsAreLoading = function() {
+    return $scope.loadingStatus;
+  };
+
+  $scope.tabIsActive = function(index) {
+    return $scope.activeTab === index;
+  };
+
+  $scope.setActiveTab = function(index) {
+    $scope.activeTab = index;
+  };
+
+  $scope.showTab = function(index) {
+
+    $scope.setActiveTab(index);
+
+    switch(index) {
+      case 1:
+        $scope.shownStreamers = $scope.allStreamers;
+        break;
+      case 2:
+        $scope.shownStreamers = $scope.onlineStreamers;
+        break;
+      case 3:
+        $scope.shownStreamers = $scope.offlineStreamers;
+        break;
+    }
+  };
+
+  // function that checks whether a user is in online user array
+  $scope.userOnline = function(user) {
+    var numStreamers = $scope.onlineStreamers.length;
+    for (var i = 0; i < numStreamers; i++) {
+      if (user.name === $scope.onlineStreamers[i].name) {
+        return true;
       }
     }
-  });
-}
+    return false;
+  };
 
-$(document).ready(function() {
-  $('#refresh').on('click', function() {
-    location.reload(true);
-  });
- 
-  // get the streams
-  for (var i = 0; i < streamers.length; i++) {
-    getStatus(streamers[i]);
+  // function that retrieves the streams based on the names in the model
+  $scope.getStreams = function() {
+
+    $scope.allStreamers = [];
+    $scope.onlineStreamers = [];
+    $scope.offlineStreamers = [];
+    $scope.adjustLoadingStatus();
+
+    $scope.streamerArr.forEach(function(streamer) {
+      var streamerUrl = $scope.baseUrl + streamer + '?callback=JSON_CALLBACK';
+      $http.jsonp(streamerUrl)
+        .success(getStreamsSuccess)
+        .error(getStreamsError.bind(streamer));
+    });
+  };
+
+  function getStreamsSuccess(response) {
+    var data = response;
+    $scope.allStreamers.push(data);
+    if (data.status === 404) {
+      data.status = 'Non-existent User';
+      $scope.offlineStreamers.push(data);
+    } else if (data.partner === false) {
+      data.status = 'User Offline';
+      $scope.offlineStreamers.push(data);
+    } else {
+      $scope.onlineStreamers.push(data);
+    }
+    $scope.adjustLoadingStatus();
+    $scope.showTab($scope.activeTab);
   }
- 
-  $('#show-all.cell').addClass('underlined-cell');
-  
-  // menu logic
-  $('#show-all').on('click', function() {
-    $('.cell').removeClass('underlined-cell');
-    $(this).addClass('underlined-cell');
-    $('.online-user').show();
-    $('.offline-user').show();
-    // give a border bottom to indicate active?
-  });
-  $('#show-online').on('click', function() {
-    $('.cell').removeClass('underlined-cell');
-    $(this).addClass('underlined-cell');
-    $('.online-user').show();
-    $('.offline-user').hide();
-  });
-  $('#show-offline').on('click', function() {
-    $('.cell').removeClass('underlined-cell');
-    $(this).addClass('underlined-cell');
-    $('.offline-user').show();
-    $('.online-user').hide();
-  });
+
+  function getStreamsError(error) {
+      console.log('Error', error.status, 'retrieving a data for streamer \'', this, '\':', error.message, '; gave error:', error.error);
+  }
+
+  // load the streams upon initialisation
+  $scope.getStreams();
+
 });
